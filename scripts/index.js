0775072040c0411aa4c5c8167d7438a5f9fb4541
chrisdev12 <christianm.bravop@gmail.com>
2020-01-31 17:21:43 -0500

    Función de paginción agregada

--- scripts/index.js
+++ scripts/index.js
@@ -2,100 +2,102 @@ const contenedor = document.querySelector('#contenedor');
 let valorBuscar = document.querySelector("#buscador");
 let buscar = document.querySelector("#buscar");
 let reset = document.querySelector("#reset");
-let counter = document.querySelector('.counter')
-const url = 'https://rickandmortyapi.com/api/'
+let counter = document.querySelector('.counter');
+// let pagination = document.querySelector('#pagination');
+let pagesButton = document.querySelectorAll('.pages-btn');
+const defaultUrl = 'https://rickandmortyapi.com/api/'
+var contadorDePagina = 1;
+
+//--------------------EVENTOS-----------------------------------------
+
+defaultView()
 
 buscar.addEventListener('click', function () {
-
-    console.log(valorBuscar.value);
-    jQuery.ajax(`${url}/character/?name=${valorBuscar.value}`, {
-        success: function (response) {
-            contenedor.innerHTML = '';
-            response.results.forEach(function (personaje) {
-                contenedor.innerHTML = contenedor.innerHTML + `<div class="personaje">
-            <img src="${personaje.image}" alt="" />
-            <h3 class="name">${personaje.name}</h3>
-            <p class="species"> ${personaje.species}</p>
-            <p class="details"> Status: ${personaje.status}</p>
-            <p class="details"> Gender: ${personaje.gender}</p>
-            <p class="details"> Origin: ${personaje.origin.name}</p>
-          </div>`
-            })
-            counter.innerHTML = `<h2 class="results"> We've founded ${response.info.count} results</h2> `
-        }
-    })
+    pageView(1)
 });
 
 reset.addEventListener('click', function () {
-    jQuery.ajax(url, {
-    success: function (response) {
-        // obteniendo personajes
-        jQuery.ajax(response.characters, {
-            success: function (response) {
-                contenedor.innerHTML = '';
-                response.results.forEach(function (personaje) {
-                    contenedor.innerHTML = contenedor.innerHTML + `<div class="personaje">
-            <img src="${personaje.image}" alt="" />
-            <h3 class="name">${personaje.name}</h3>
-            <p class="species"> ${personaje.species}</p>
-            <p class="details"> Status: ${personaje.status}</p>
-            <p class="details"> Gender: ${personaje.gender}</p>
-            <p class="details"> Origin: ${personaje.origin.name}</p>
-          </div>`
-                })
-                
-                 counter.innerHTML = `<h2 class="results"> Search your favorite Rick&Morty character by their name </h2> `
-            },
-            error: function (error) {
-                console.error('Error trayendo personajes ', error)
-            }
-        })
-    },
-    error: function (error) {
-        console.error('Error en el request', error)
-    }
-});
+   contenedor.innerHTML = ''; //Repinto el innerHTML desde ;
+   contadorDePagina = 0; //Reinicio el contador:
+   defaultView();
 });
 
-
-//---------------------------------------------------------------------
-
-jQuery.ajax(url, {
-    success: function (response) {
-        // obteniendo personajes
-        jQuery.ajax(response.characters, {
-            success: function (response) {
-                response.results.forEach(function (personaje) {
-                    contenedor.innerHTML = contenedor.innerHTML + `<div class="personaje">
-            <img src="${personaje.image}" alt="" />
-            <h3 class="name">${personaje.name}</h3>
-            <p class="species"> ${personaje.species}</p>
-            <p class="details"> Status: ${personaje.status}</p>
-            <p class="details"> Gender: ${personaje.gender}</p>
-            <p class="details"> Origin: ${personaje.origin.name}</p>
-          </div>`
-                })
-            },
-            error: function (error) {
-                console.error('Error trayendo personajes ', error)
-            }
-        })
-    },
-    error: function (error) {
-        console.error('Error en el request', error)
-    }
-});
-
-
-
- 
-
+pagesButton.forEach(element => {
     
+    element.addEventListener('click', function () {
+        
+        //Esta parte maneja de forma entera la paginación, envia como parametro el número de pagina que se le
+        //requiere al API.
+        
+        switch (event.target.value) {
+            case 'retroceder':
+                contadorDePagina--;
+                pageView(contadorDePagina);
+                break;
+            case 'avanzar':
+                contadorDePagina++;
+                pageView(contadorDePagina);            
+                break;
+            default: console.log('Error en la sección de botones de página');
+                break;
+        }
+    });
+    
+});
 
 
+//----------------------FUNCIONES---------------------------------------
 
+function defaultView() {
+    jQuery.ajax(defaultUrl, {
+    success: function (response) {
+        // obteniendo personajes
+        jQuery.ajax(response.characters, {
+            success: function (response) {
+                response.results.forEach(function (personaje) {
+                    contenedor.innerHTML = contenedor.innerHTML + `<div class="personaje">
+                    <img src="${personaje.image}" alt="" />
+                    <h3 class="name">${personaje.name}</h3>
+                    <p class="species"> ${personaje.species}</p>
+                    <p class="details"> Status: ${personaje.status}</p>
+                    <p class="details"> Gender: ${personaje.gender}</p>
+                    <p class="details"> Origin: ${personaje.origin.name}</p></div>`
+                })
+                counter.innerHTML = `<h2 class="results"> 
+                 Search your favorite Rick&Morty character by their name </h2> `;
+            },
+            error: function (error) {
+                console.error('Error trayendo personajes ', error)
+            }
+        })
+    },
+    error: function (error) {
+        console.error('Error en el request', error)
+    }
+});
+};
 
-// console.log($.get(`https://rickandmortyapi.com/api/character/?name=rick`,
-//     function (data, status) {
-//     console.log(data);
-// }))
+function pageView(pagina) {
+    
+    let pageRequested = pagina;
+    console.log(pageRequested);
+    let url = `https://rickandmortyapi.com/api/character?page=${pageRequested}&name=${valorBuscar.value}`          
+    jQuery.ajax(url, {
+        success: function (response) {
+            console.log(response);
+            contenedor.innerHTML = '';
+            response.results.forEach(function (personaje) {
+                contenedor.innerHTML = contenedor.innerHTML + `<div class="personaje">
+                <img src="${personaje.image}" alt="" />
+                <h3 class="name">${personaje.name}</h3>
+                <p class="species"> ${personaje.species}</p>
+                <p class="details"> Status: ${personaje.status}</p>
+                <p class="details"> Gender: ${personaje.gender}</p>
+                <p class="details"> Origin: ${personaje.origin.name}</p></div>`
+            })
+            counter.innerHTML = `<h2 class="results"> 
+            We've founded ${response.info.count} results</h2> `;
+        }
+    });
+};
+ 
